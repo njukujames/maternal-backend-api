8f7130a3f08adfc23238cd2e3bfde1b24a9b1f0e
manukanja <manukanja@outlook.com>
2025-12-04 19:05:05 +0300

    updated schemas __inti__.py

---
+++ app/schemas/__init__.py
@@ -0,0 +1,10 @@
+"""
+Pydantic Schemas Package
+Contains request/response models and data validation schemas.
+"""
+
+# TODO: Import schemas as they are implemented
+# from .user import UserCreate, UserResponse, PregnancyProfile
+# from .logs import DailyLogCreate, SymptomLog, MoodLog, WeightLog
+# from .content import WeeklyContent, SymptomGuide
+# from .feedback import FeedbackCreate
