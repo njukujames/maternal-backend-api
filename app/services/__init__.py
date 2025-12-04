45fb6b24825c9c2039c6eba955e9d8f8ac26749f
manukanja <manukanja@outlook.com>
2025-12-04 18:59:11 +0300

    added and updated services __init__.py

---
+++ app/services/__init__.py
@@ -0,0 +1,10 @@
+"""
+Services Package
+Contains business logic and helper functions.
+"""
+
+# TODO: Implement services as needed
+# from .auth_service import AuthService
+# from .pregnancy_calculator import PregnancyCalculator
+# from .notification_service import NotificationService
+# from .pdf_generator import PDFGenerator
