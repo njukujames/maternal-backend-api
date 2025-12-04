750c73cd639bd63218c93be040577d9e15652ac0
manukanja <manukanja@outlook.com>
2025-12-04 19:03:01 +0300

    added and updated the routers __init__.py

---
+++ app/routers/__init__.py
@@ -0,0 +1,7 @@
+"""
+API Routers Package
+Contains all endpoint route handlers for the application.
+"""
+
+# TODO: Import routers as they are implemented
+# from . import auth, users, logs, content, feedback
